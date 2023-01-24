d6a53bc9646facb6e6e4fb75d680d1ff087cc781
Jaafar Zubaidi <jaafarwerdi@proton.me>
2023-01-24 13:22:34 -0500

    Initial Commit

---
+++ HardJSONDecodingAssignment/HardJSONDecodingAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HardJSONDecodingAssignmentApp.swift
+//  HardJSONDecodingAssignment
+//
+//  Created by Jaafar Zubaidi  on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct HardJSONDecodingAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
